fc54eb6fef1ba8fd6f17bd9cdb11f424ff3ab781
Matias Mariani <hi@matiasmariani.io>
2017-02-08 10:37:12 -0500

    finished reviews. styling about book

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,14 +3,11 @@ var Handlebars = require('Handlebars');
 
 $(document).ready(function(){
      $('.parallax').parallax();
+     $('.modal').modal();
+     console.log();
    });
 
 //######## PROJECTS HANDLEBARS ##########//
 var source = $("#reviewsTemplate").html();
 var template = Handlebars.compile(source);
 $('#reviewsContainer').html(template(reviews));
-
-$(document).ready(function(){
-   $('.modal').modal();
-   console.log(this);
- });
--- app/scripts/reviews.js
+++ app/scripts/reviews.js
@@ -4,7 +4,7 @@ var reviews = {
         snippet: 'Karen Moore is among the most innovative and strategic professionals in the communications industry and is setting a new standard for what advocacy means. With a true passion for advocacy, Karen empowers those',
         review: 'Karen Moore is among the most innovative and strategic professionals in the communications industry and is setting a new standard for what advocacy means. With a true passion for advocacy, Karen empowers those that she interacts with in a tremendously thoughtful manner. Her ability to make connections with determination allows her to shape the conversation with engaged voices, leading the charge surrounding our most critical patient-access issues. Her insights and guidance are well recognized and sought after by the individuals and organizations across the industry',
         reviewer: 'Scott LaGanga',
-        position: 'senior vice president of Advocacy and Strategic Alliances, Pharmaceutical Research and Manufacturers of America (PhRMA)',
+        position: 'Senior vice president of Advocacy and Strategic Alliances, Pharmaceutical Research and Manufacturers of America (PhRMA)',
     }, {
       id: '#2',
       snippet: 'When it comes to winning advocacy battles, Karen B. Moore is the undisputed leader. She is able to easily reach Hispanic audiences and move them to action, making a difference at',
