33d9127b7166919b13279fbe98ba207f089f10ec
Matias Mariani <hi@matiasmariani.io>
2017-02-08 09:53:36 -0500

    styling

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,16 @@
-console.log("Hello World!");
+var reviews = require('../scripts/reviews.js').reviews;
+var Handlebars = require('Handlebars');
+
 $(document).ready(function(){
      $('.parallax').parallax();
    });
+
+//######## PROJECTS HANDLEBARS ##########//
+var source = $("#reviewsTemplate").html();
+var template = Handlebars.compile(source);
+$('#reviewsContainer').html(template(reviews));
+
+$(document).ready(function(){
+   $('.modal').modal();
+   console.log(this);
+ });
---
+++ app/scripts/reviews.js
@@ -0,0 +1,24 @@
+var reviews = {
+    data: [{
+        id: '#1',
+        snippet: 'Karen Moore is among the most innovative and strategic professionals in the communications industry and is setting a new standard for what advocacy means. With a true passion for advocacy, Karen empowers those',
+        review: 'Karen Moore is among the most innovative and strategic professionals in the communications industry and is setting a new standard for what advocacy means. With a true passion for advocacy, Karen empowers those that she interacts with in a tremendously thoughtful manner. Her ability to make connections with determination allows her to shape the conversation with engaged voices, leading the charge surrounding our most critical patient-access issues. Her insights and guidance are well recognized and sought after by the individuals and organizations across the industry',
+        reviewer: 'Scott LaGanga',
+        position: 'senior vice president of Advocacy and Strategic Alliances, Pharmaceutical Research and Manufacturers of America (PhRMA)',
+    }, {
+      id: '#2',
+      snippet: 'When it comes to winning advocacy battles, Karen B. Moore is the undisputed leader. She is able to easily reach Hispanic audiences and move them to action, making a difference at',
+      review: 'When it comes to winning advocacy battles, Karen B. Moore is the undisputed leader. She is able to easily reach Hispanic audiences and move them to action, making a difference at every turn. Her in-depth knowledge of the target audience and the legislative process have been perfectly honed to serve organizations across the globe.',
+      reviewer: 'Julio Fuentes',
+      position: 'President, Florida State Hispanic Chamber of Commerce',
+    }, {
+      id: '#3',
+      snippet: 'Karen Moore is the top advocacy powerhouse. From impactful messaging to effective grassroots activities, she is the best in the business. Her strategies',
+      review: 'Karen Moore is the top advocacy powerhouse. From impactful messaging to effective grassroots activities, she is the best in the business. Her strategies are powerful, and the results are guaranteed. If you could read only one book before engaging in advocacy, this is the must-read primer.',
+      reviewer: 'Rene Rodriguez',
+      position: 'MD, president, Salud USA',
+    }]
+};
+module.exports = {
+    reviews: reviews
+};
