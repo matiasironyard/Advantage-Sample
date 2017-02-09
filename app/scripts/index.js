be928d263a34eab20ed561f50318d1806c5ab579
Matias Mariani <hi@matiasmariani.io>
2017-02-08 20:53:41 -0500

    styling

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,8 @@ var reviews = require('../scripts/reviews.js').reviews;
 var Handlebars = require('Handlebars');
 
 $(document).ready(function(){
+    Materialize.fadeInImage('body');
+    $('.scrollspy').scrollSpy();
      $('.parallax').parallax();
      $('.modal').modal();
      $('.slider').slider({indicators: false, height: 500, transition: 800});
@@ -12,7 +14,13 @@ $(document).ready(function(){
       draggable: true,
         }
       );
-
+      var options = [
+        {selector: '#reviews', offset: 500, callback: function() {
+          $('#reviews').removeClass('hidden');
+          Materialize.fadeInImage('#reviews');
+        } },
+      ];
+      Materialize.scrollFire(options);
    });
 
 //######## PROJECTS HANDLEBARS ##########//
