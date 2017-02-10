22ac49431b5279917648fd90e30c9821d266b779
Matias Mariani <hi@matiasmariani.io>
2017-02-10 03:01:59 -0500

    clean up

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,46 +1,49 @@
 var reviews = require('../scripts/reviews.js').reviews;
 var Handlebars = require('Handlebars');
-
-$(document).ready(function(){
-    Materialize.fadeInImage('body');
-     $('.parallax').parallax();
-     $('.modal').modal();
-     $('.slider').slider({indicators: false, height: 650, transition: 800});
-     $('.button-collapse').sideNav({
-      menuWidth: 300,
-      edge: 'left',
-      closeOnClick: true,
-      draggable: true,
-        }
-      );
-      var options = [
-        {selector: '#about-book', offset: 20, callback: function() {
-          // $('#about-book').removeClass('hidden');
-          Materialize.fadeInImage('#about-book');
-        } },
+$(document).ready(function() {
+  Materialize.fadeInImage('body');
+  $('.parallax').parallax();
+  $('.modal').modal();
+  $('.slider').slider({
+    indicators: false,
+    height: 650,
+    transition: 800
+  });
+  $('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true,
+  });
+  var options = [
+    {
+      selector: '#about-book',
+      offset: 20,
+      callback: function() {
+        // $('#about-book').removeClass('hidden');
+        Materialize.fadeInImage('#about-book');
+      }
+      },
       ];
-      Materialize.scrollFire(options);
-   });
-
+  Materialize.scrollFire(options);
+});
 //###########VIDEO############//
- $(document).ready(function(){
-   $('.fa-angle-down').click(function(){
-     $('#come-look').fadeOut(100);
-     $('#video').fadeIn(3000);
-   });
- });
-
+$(document).ready(function() {
+  $('.fa-angle-down').click(function() {
+    $('#come-look').fadeOut(100);
+    $('#video').fadeIn(3000);
+  });
+});
 //######## PROJECTS HANDLEBARS ##########//
 var source = $("#reviewsTemplate").html();
 var template = Handlebars.compile(source);
 $('#reviewsContainer').html(template(reviews));
-
 //###########SCROLL###############//
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
@@ -50,9 +53,7 @@ $(function() {
     }
   });
 });
-
 //######## PROJECTS HANDLEBARS ##########//
-
 // function isInView(elem){
 //    return $(elem).offset().top - $(window).scrollTop() < $(elem).height() ;
 // }
