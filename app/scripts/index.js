12e255566c15c0fcfbb06a82c577cebc9785c985
Matias Mariani <hi@matiasmariani.io>
2017-02-10 02:52:01 -0500

    redistribution

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,7 +3,6 @@ var Handlebars = require('Handlebars');
 
 $(document).ready(function(){
     Materialize.fadeInImage('body');
-    $('.scrollspy').scrollSpy();
      $('.parallax').parallax();
      $('.modal').modal();
      $('.slider').slider({indicators: false, height: 650, transition: 800});
@@ -36,6 +35,21 @@ var source = $("#reviewsTemplate").html();
 var template = Handlebars.compile(source);
 $('#reviewsContainer').html(template(reviews));
 
+//###########SCROLL###############//
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+    }
+  });
+});
 
 //######## PROJECTS HANDLEBARS ##########//
 
